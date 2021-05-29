867302ae8ee10b6ffd9e9fed6baa6359cba1be65
Rosun GV <gvrosun@gmail.com>
2021-05-29 09:31:36 +0530

    Added more functionality

--- app.py
+++ app.py
@@ -2,11 +2,35 @@
 
 import sys
 import os
+import json
+from termcolor import cprint
 
-words = sys.argv
-if len(words) > 1:
-    words.pop(0)
-    print(words)
-else:
-    print("Invalid syntax!")
-    print("Syntax: python3 app.py word1 word2 etc")
+
+class Dictionary:
+    def __init__(self):
+        self.words = set(item.lower() for item in sys.argv[1:])
+        if not self.words:
+            cprint("[-] Invalid syntax!", 'red')
+            cprint("[-] Syntax: python3 app.py word1 word2 etc", 'red')
+            sys.exit()
+        if os.path.exists("data/data.json"):
+            self.data = json.load(open("data/data.json"))
+        else:
+            print("[-] Data file not found!", 'red')
+            sys.exit()
+
+    def find(self):
+        for word in self.words:
+            cprint('-' * 50, color='yellow')
+            cprint(f'[+] {word.capitalize()}', color='green', attrs=['bold'])
+            meaning = self.data.get(word)
+            if meaning:
+                for item in meaning:
+                    cprint(f'-> {item}', 'cyan')
+            else:
+                cprint("!!! Please double check the word", color='red')
+        cprint('-' * 50, color='yellow')
+
+
+my_dict = Dictionary()
+my_dict.find()
