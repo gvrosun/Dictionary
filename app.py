aa5a9d00a80af980bfac21e044a82328169264ee
Rosun GV <gvrosun@gmail.com>
2021-05-29 10:50:49 +0530

    Fully functional program

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import sys
 import os
 import json
 from termcolor import cprint
+from difflib import get_close_matches
 
 
 class Dictionary:
     def __init__(self):
+        os.system('cls||clear')
+        print('Running...')
         self.words = set(item.lower() for item in sys.argv[1:])
         if not self.words:
             cprint("[-] Invalid syntax!", 'red')
@@ -19,18 +22,46 @@ class Dictionary:
             print("[-] Data file not found!", 'red')
             sys.exit()
 
-    def find(self):
-        for word in self.words:
+    def find_unknown(self, u_words):
+        for word in u_words:
             cprint('-' * 50, color='yellow')
-            cprint(f'[+] {word.capitalize()}', color='green', attrs=['bold'])
+            possibilities = get_close_matches(word, self.data.keys())
+            if len(possibilities) == 0:
+                cprint(f"[-] Please double check the word {word}", color='red')
+                continue
+            possibilities.append('None')
+            cprint(f'[*] {word}', 'yellow')
+            for index, item in enumerate(possibilities):
+                print(f'{index}. {item}')
+            print('Please select correct word: ', end="")
+            choice = int(input())
+            sys.stdout.write("\033[F" * (len(possibilities) + 2))
+            if possibilities[-1] == possibilities[choice]:
+                cprint(f"!!! Sorry unable to find word {word}", color='red')
+            else:
+                cprint(f'[+] {possibilities[choice].capitalize()}', color='green')
+                meaning = self.data.get(possibilities[choice])
+                for item in meaning:
+                    cprint(f'-> {item}', 'cyan')
+        cprint('-' * 50, color='yellow')
+
+    def find(self):
+        unknown_words = []
+        for word in self.words:
             meaning = self.data.get(word)
             if meaning:
+                cprint('-' * 50, color='yellow')
+                cprint(f'[+] {word.capitalize()}', color='green')
                 for item in meaning:
                     cprint(f'-> {item}', 'cyan')
             else:
-                cprint("!!! Please double check the word", color='red')
-        cprint('-' * 50, color='yellow')
+                unknown_words.append(word)
+        if unknown_words:
+            self.find_unknown(unknown_words)
+        else:
+            cprint('-' * 50, color='yellow')
 
 
 my_dict = Dictionary()
 my_dict.find()
+print('Stopped...')
